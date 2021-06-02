4957e9df80dcdaec6a63fd6fcc6a59c267dd2568
NinmarAcar <73597589+NinmarAcar@users.noreply.github.com>
2021-06-02 22:17:24 +0200

    Updates

---
+++ precache-manifest.45fea6a65746fa2c8dec861fb85434b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2dd9a84c8fec68978f3f8ed9b18d5d1",
+    "url": "/CvNinmar/index.html"
+  },
+  {
+    "revision": "f311903d2e1f0fa8508c",
+    "url": "/CvNinmar/static/css/2.56e33495.chunk.css"
+  },
+  {
+    "revision": "b3c4d8a48b2155cca5f6",
+    "url": "/CvNinmar/static/css/main.12268149.chunk.css"
+  },
+  {
+    "revision": "f311903d2e1f0fa8508c",
+    "url": "/CvNinmar/static/js/2.e8013c51.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/CvNinmar/static/js/2.e8013c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3c4d8a48b2155cca5f6",
+    "url": "/CvNinmar/static/js/main.6d01e6a4.chunk.js"
+  },
+  {
+    "revision": "a4e2ee13c331e9f0c081",
+    "url": "/CvNinmar/static/js/runtime-main.3f700bcd.js"
+  }
+]);
